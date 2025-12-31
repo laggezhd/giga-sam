a9f252f31ef57e5aa8841c6d24fbe8256d288f31
laggezhd <sschwartz@student.ethz.ch>
2025-12-31 17:00:44 +0100

    update mIoU function and add visualization function

--- src/minimal_sam/utils/helper.py
+++ src/minimal_sam/utils/helper.py
@@ -1,9 +1,11 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
 from PIL import Image
 from ptflops import get_model_complexity_info
 from torchvision import transforms
+from torchmetrics.segmentation import MeanIoU
 from time import perf_counter
 from tqdm import tqdm
 
@@ -29,47 +31,60 @@ def overlay(image, mask, color=(0, 0, 255), alpha=0.5):
     return overlay.astype(np.uint8)
 
 
-def compute_batch_counts(logits: torch.Tensor, targets: torch.Tensor, threshold=0.5):
-    assert torch.all((targets == 0) | (targets == 1)), "Targets must be binary (0 or 1)"
-    assert logits.shape == targets.shape, "Logits and targets must have the same shape"
-
-    preds = (torch.sigmoid(logits) > threshold)
-    targets = (targets > 0.5) 
-
-    # Summing over all dimensions (Batch, C, H, W) gives the total pixel count
-    intersection = (preds & targets).long().sum().item()
-    union = (preds | targets).long().sum().item()
-    
-    return intersection, union
-      
-
-def measure_global_iou(model, dataloader=None, device='cuda', threshold=0.5):
-    if dataloader is None:
-        print("No dataloader provided for global IoU measurement.")
-        return None
-    
-    total_intersection = 0.0
-    total_union = 0.0
-
+def show_visualization(model, dataset, device='cpu', start_idx=0, skip_idx=1, num_samples=20, threshold=0.5):
     model.to(device)
     model.eval()
+
+    for i in range(num_samples):
+        image_tensor, mask_tensor = dataset[start_idx + (i * skip_idx)]
+        input_tensor = image_tensor.unsqueeze(0).to(device)  # add batch dimension
+
+        with torch.no_grad():
+            output = model(input_tensor)
+
+        pred_mask = (torch.sigmoid(output) > threshold).float().squeeze(0).cpu()
+
+        fig, axs = plt.subplots(1, 2, figsize=(12, 4))
+        axs[0].set_title("Original Image + Mask")
+        axs[0].imshow(overlay(get_img_from_tensor(image_tensor), mask_tensor))
+        axs[0].axis("off")
+        axs[1].set_title("Predicted Mask")
+        axs[1].imshow(overlay(get_img_from_tensor(image_tensor), pred_mask))
+        axs[1].axis("off")
+        plt.show()
+    
+
+
+def measure_global_iou(model, dataloader, device='cuda'):
+    print(f"Measuring global IoU on '{device}'...")
+
+    if device == 'cuda' and not torch.cuda.is_available():
+        print("CUDA is not available. Falling back to CPU.")
+        device = 'cpu'
+    
+    model.to(device)
+    model.eval()
+
+    miou = MeanIoU(num_classes=2, include_background=True, per_class=True, input_format='index').to(device)
 
     with torch.no_grad():
         for images, masks in tqdm(dataloader):
             images, masks = images.to(device), masks.to(device)
-    
+
             logits = model(images)
+            preds = (torch.sigmoid(logits) > 0.5)
 
-            inter, union = compute_batch_counts(logits, masks, threshold)
+            miou.update(preds.long(), masks.long())
 
-            total_intersection += inter
-            total_union += union
+    global_miou = miou.compute()
 
-    global_iou = total_intersection / (total_union + 1e-6)
-    
-    print(f"Global mIoU: {global_iou:.4f}")
-    
-    return global_iou
+    bg_iou = global_miou[0].item()
+    fg_iou = global_miou[1].item()
+
+    print(f"Background IoU: {bg_iou:.4f}")
+    print(f"Foreground IoU: {fg_iou:.4f}")
+
+    return bg_iou, fg_iou
 
 
 def measure_macs_params(model, input_size=(3, 96, 96)):
@@ -82,7 +97,7 @@ def measure_macs_params(model, input_size=(3, 96, 96)):
 
 
 def measure_inference_time(model, input_size=(3, 96, 96), device='cuda', iterations=500):
-    print(f"Measuring inference time on device: {device}")
+    print(f"Measuring inference time on '{device}' over {iterations} iterations...")
 
     if device == 'cuda' and not torch.cuda.is_available():
         print("CUDA is not available. Falling back to CPU.")
@@ -132,9 +147,9 @@ def measure_inference_time(model, input_size=(3, 96, 96), device='cuda', iterati
 
 def benchmark(model, dataloader=None, device='cpu', input_size=(1, 3, 96, 96), iterations=500):
     print("=== Benchmark Summary ===")
-    macs, params = measure_macs_params(model, input_size=input_size[1:])
-    t_avg, t_std, t_p99 = measure_inference_time(model, input_size=input_size, device=device, iterations=iterations)
-    global_iou = measure_global_iou(model, dataloader, device)
+    macs, params = measure_macs_params(model, input_size[1:])
+    t_avg, t_std, t_p99 = measure_inference_time(model, input_size, device, iterations)
+    bg_iou, fg_iou = measure_global_iou(model, dataloader, device)
     print("=========================\n")
 
     return {
@@ -143,5 +158,6 @@ def benchmark(model, dataloader=None, device='cpu', input_size=(1, 3, 96, 96), i
         "inference_time_avg_ms": t_avg,
         "inference_time_std_ms": t_std,
         "inference_time_p99_ms": t_p99,
-        "global_iou": global_iou
+        "bg_iou": bg_iou,
+        "fg_iou": fg_iou
     }

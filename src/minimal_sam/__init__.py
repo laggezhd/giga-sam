c28185da0047212ecf9474125e8d570675cc12e2
laggezhd <sschwartz@student.ethz.ch>
2026-01-05 14:24:39 +0100

    delete old models

--- src/minimal_sam/__init__.py
+++ src/minimal_sam/__init__.py
@@ -1,4 +1,4 @@
-from .models import MicroSAM, PicoSAM, MinimalSAM
+from .models import MicroSAM, PicoSAM
 from .utils.data import MinimalSamDataset
 from .utils.loss import bce_dice_loss
 from .utils.metrics import compute_iou
